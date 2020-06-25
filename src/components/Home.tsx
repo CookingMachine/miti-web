86049a92da3ae26b8c0bbec89fea6cbe270c687d
Exide <ig.roman95@mail.ru>
2020-06-26 00:38:26 +0300

    Initial commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+
+interface Props extends RouteComponentProps {
+}
+
+const HomePage = ({}: Props) => {
+
+    return (
+        <div style={{textAlign: 'center', marginTop: '300px'}}>
+            <div style={{display: 'inline-block', width: '350px'}}>
+                <h1>Hi Mark</h1>
+                <input width='100%'/>
+            </div>
+        </div>
+    )
+}
+
+export default withRouter<Props, typeof HomePage>(HomePage);
