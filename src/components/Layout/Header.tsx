a6801403771a5e820b1c4c4609141ac2023bcaaa
Denis Prokopenkov <oprokopenkova8903@list.ru>
2020-06-27 02:37:19 +0300

    added header markup

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Row, Col } from 'reactstrap';
 
 interface Props {
     
@@ -6,9 +7,27 @@ interface Props {
 
 const Header = ({}: Props) => {
     return (
-        <div className='header'>
-            Header
-        </div>
+       <Row className="header">
+            <Col md={4}>
+                <div className="logo">
+                    <p>
+                        Here will logo!
+                    </p>
+                </div>
+            </Col>
+            <Col md={4}>
+            </Col>
+            <Col md={4}>
+                <div className="headerLinks">
+                    <a href="" className="mapLink">  {/* TODO u use link instead of teg 'a' */}
+                        Карта сайта
+                    </a>
+                    <a href="" className="feddbackLink">
+                        Обратная связь
+                    </a>
+                </div>
+            </Col>           
+       </Row>
     )
 }
 
